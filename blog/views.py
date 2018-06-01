7a7c954f30ef920374863dc14005c18584a3fc34
Silver <baetsilverjan.dpc@gmail.com>
2018-06-01 16:54:22 +0800

    changed template

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from djang.utils import timezone
+from django.utils import timezone
 from django.shortcuts import render
 from .models import Post
 # Create your views here.
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now().order_by('created_date'))
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
