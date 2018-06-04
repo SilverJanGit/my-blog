802f1fcd21e9358f7451e014b7c714827ef0f7af
Silver <baetsilverjan.dpc@gmail.com>
2018-06-04 11:43:08 +0800

    Added database.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import url
 from . import views
-from django.shortcuts import render, get_object_or_404
-
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
